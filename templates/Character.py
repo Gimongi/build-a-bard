85fea26c747e27304f749dec3228d570cc03f0ac
Trevor Eames <troves@Trevors-MacBook-Pro.local>
2021-12-10 20:49:20 +0000

    adding more fields to character

--- templates/Character.py
+++ templates/Character.py
@@ -1,5 +1,6 @@
 from templates.CharacterAbilities import CharacterAbilities
 from utils.Abilities import Abilities
+from utils.Alignment import Alignment
 from utils.CharClass import CharClass
 from utils.CharRace import CharRace
 from utils.Utils import Utils
@@ -7,16 +8,16 @@ from utils.Utils import Utils
 
 class Character:
     level = 0
-    
-    # charClass: CharClass
-    # charRace: CharRace
-
     health = 0
     hitDie = 0
+    # age = 0
+    speed = 0
+    languages = []
 
-    # abilities: CharacterAbilities
-    # features = CharacterFeatures()
-    # spells = CharacterSpells()
+    features = []
+    spells = []
+    
+    description = ""
 
     def __init__(self, charClass, charRace, level, tags):
         self.charClass = CharClass.fromStr(charClass)
@@ -29,11 +30,14 @@ class Character:
     def initialiseChar(self):
         self.hitDie = self.abilities.hitDie
         self.health = self.hitDie + Utils.getModifier(self.abilities.constitution)
+        self.applyRaceToCharacter()
+
 
     def levelUpChar(self):
         # call all the leveling up methods in abilities, features, spells
         pass
 
+    
     def characterInfo(self):
         print("Hi I am a " + self.charRace + " " + self.charClass + ". I am level " + str(self.level))
         print("My abilities are: ")
@@ -46,3 +50,105 @@ class Character:
         print("My health is currently " + str(self.health) + ", and my hit die is ") + str(self.hitDie)
         print("My saving throws are: " + str(self.abilities.saves))
 
+
+    def applyRaceToCharacter(self):
+        if self.charRace == CharRace.aarakocra:
+            self.abilities.dexterity += 2
+            self.abilities.wisdom += 1
+
+            self.speed = 25
+            self.alignment = Alignment.neutral_good
+
+            self.languages.append("Common")
+            self.languages.append("Aarakocra")
+            self.languages.append("Auran")
+
+            # TODO: description should be of the character (colour/etc.)? or of the race itself. Not sure. Probably of character
+            # self.description = 
+            pass
+        elif self.charRace == CharRace.aasimar:
+            pass
+        elif self.charRace == CharRace.bugbear:
+            pass
+        elif self.charRace == CharRace.centaur:
+            pass
+        elif self.charRace == CharRace.changeling:
+            pass
+        elif self.charRace == CharRace.dragonborn:
+            pass
+        elif self.charRace == CharRace.dwarf:
+            pass
+        elif self.charRace == CharRace.elf:
+            pass
+        elif self.charRace == CharRace.fairy:
+            pass
+        elif self.charRace == CharRace.firbolg:
+            pass
+        elif self.charRace == CharRace.genasi:
+            pass
+        elif self.charRace == CharRace.gith:
+            pass
+        elif self.charRace == CharRace.gnome:
+            pass
+        elif self.charRace == CharRace.goblin:
+            pass
+        elif self.charRace == CharRace.goliath:
+            pass
+        elif self.charRace == CharRace.grung:
+            pass
+        elif self.charRace == CharRace.half_elf:
+            pass
+        elif self.charRace == CharRace.halfling:
+            pass
+        elif self.charRace == CharRace.half_orc:
+            pass
+        elif self.charRace == CharRace.harengon:
+            pass
+        elif self.charRace == CharRace.hobgoblin:
+            pass
+        elif self.charRace == CharRace.human:
+            pass
+        elif self.charRace == CharRace.kalashtar:
+            pass
+        elif self.charRace == CharRace.kenku:
+            pass
+        elif self.charRace == CharRace.kobold:
+            pass
+        elif self.charRace == CharRace.leonin:
+            pass
+        elif self.charRace == CharRace.lizardfolk:
+            pass
+        elif self.charRace == CharRace.locathah:
+            pass
+        elif self.charRace == CharRace.loxodon:
+            pass
+        elif self.charRace == CharRace.minotaur:
+            pass
+        elif self.charRace == CharRace.orc:
+            pass
+        elif self.charRace == CharRace.owlin:
+            pass
+        elif self.charRace == CharRace.satyr:
+            pass
+        elif self.charRace == CharRace.shifter:
+            pass
+        elif self.charRace == CharRace.simic_hybrid:
+            pass
+        elif self.charRace == CharRace.tabaxi:
+            pass
+        elif self.charRace == CharRace.tiefling:
+            pass
+        elif self.charRace == CharRace.tortle:
+            pass
+        elif self.charRace == CharRace.triton:
+            pass
+        elif self.charRace == CharRace.vedalken:
+            pass
+        elif self.charRace == CharRace.verdan:
+            pass
+        elif self.charRace == CharRace.warforged:
+            pass
+        elif self.charRace == CharRace.yuan_ti:
+            pass
+        else:
+            raise NotImplementedError
